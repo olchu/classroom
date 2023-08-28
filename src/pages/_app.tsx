d59f01c8c47691d9c8afd7c9f837f3a7bd930bf2
Oleg Churkin <olchu30@yandex.ru>
2023-08-28 21:51:51 +0300

    first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,17 @@
+import { ReactElement } from 'react'
+import type { AppProps } from 'next/app'
+import PageWithLayoutType from '@/types/pageWithLayouts'
+type AppLayoutProps = AppProps & {
+  Component: PageWithLayoutType
+  pageProps: any
+}
+function MyApp({ Component, pageProps }: AppLayoutProps) {
+  const Layout =
+    Component.layout || ((children: ReactElement) => <>{children}</>)
+return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
+}
+export default MyApp
---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import MainLayout from "@/components/layouts/main"
+
+const Home = () => {
+  return <div>home</div>
+}
+Home.layout = MainLayout
+export default Home
