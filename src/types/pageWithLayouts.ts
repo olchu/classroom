71b1fae1af1100efc3bbf292f27a067879096861
Oleg Churkin <olchu30@yandex.ru>
2023-08-28 22:25:11 +0300

    add db conection & gen schema

--- src/types/pageWithLayouts.ts
+++ src/types/pageWithLayouts.ts
@@ -1,13 +1,9 @@
-import AdminLayout from '@/components/layouts/admin';
 import MainLayout from '@/components/layouts/main';
 import { NextPage } from 'next';
 import { ReactElement } from 'react';
 
 export type PageWithMainLayoutType = NextPage & { layout: typeof MainLayout };
-export type PageWithAdminLayoutType = NextPage & { layout: typeof AdminLayout };
-export type PageWithLayoutType =
-  | PageWithMainLayoutType
-  | PageWithAdminLayoutType;
+export type PageWithLayoutType = PageWithMainLayoutType;
 export type LayoutProps = ({
   children,
 }: {
